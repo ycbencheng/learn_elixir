319b44d840dde9804da2a9617a70ce2e9f58b308
Ben Cheng @ Divvy <ben.cheng@divvypay.com>
2022-07-21 10:12:27 -0600

    Update doc

--- cards_demo/lib/cards.ex
+++ cards_demo/lib/cards.ex
@@ -26,6 +26,14 @@ defmodule Cards do
   @doc """
     Divides a deck into a hand and the remainder of the deck.
     The `hand_size` argument indicates how many cards should be in the hand.
+
+  ## Examples
+    
+      iex>  deck = Cards.create_deck
+      iex>  {hand, deck} = Cards.deal(deck, 1)
+      iex>  hand
+      ["Ace of Spades "]
+
   """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
