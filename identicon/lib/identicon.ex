2dda690d08bd89c033f589e60ce247dd95b55c6a
Ben Cheng @ Divvy <ben.cheng@divvypay.com>
2022-08-08 17:10:52 -0600

    Create pixel_map method

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -9,6 +9,7 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
   end
 
   def hash_input(input) do
@@ -48,6 +49,22 @@ defmodule Identicon do
     grid = Enum.filter(grid, fn({code, _index}) -> rem(code, 2) end)
 
     %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(image) do
+    %Identicon.Image{grid: grid} = image
+
+    pixel_map = Enum.map(grid, fn({_code, index}) -> 
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 end
  
--- identicon/lib/image.ex
+++ identicon/lib/image.ex
@@ -2,5 +2,5 @@ defmodule Identicon.Image do
   @moduledoc """
   Documentation for `Identicon.Image`.
   """
-  defstruct hex: nil, color: nil, grid: nil
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
